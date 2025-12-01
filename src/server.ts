b8597d45509b909973601666b0f0e98887f4c70f
Aissa <aissa.allay@gmail.com>
2025-12-01 12:38:28 +0100

    Servidor corriendo, index y estilo integrado

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,15 @@
-import {Request, Response} from 'express';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const app = express();
+app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../public')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server esta corriendo en http://localhost:${PORT}`);
+});
