f63ade440bcd93cf8886414914aa75b00cab876a
Aissa <aissa.allay@gmail.com>
2025-12-02 21:05:08 +0100

    Integración de controllers y calculator de compatibilidad mejorada

--- src/controllers/match.controller.ts
+++ src/controllers/match.controller.ts
@@ -4,5 +4,17 @@ import {trips} from '../data/trip.data';
 import {calculatorMatch } from '../utils/match';
 
 export const matchUserTrip = (req: Request, res: Response) => {
-    
+    const {userId, tripId} = req.body;
+
+    const user = users.find(u=>u.id === userId);
+    const trip = trips.find(f=>f.id === tripId);
+
+    if(!user || !trip){
+        return res.status(404).json({message: "Usuario o viaje no encontrado"});
+    }
+
+    const result = calculatorMatch(user, trip);
+
+    return res.json(result);
+
 };
--- src/utils/match.ts
+++ src/utils/match.ts
@@ -2,5 +2,14 @@ import {User} from '../models/user.model';
 import {Trip} from '../models/trip.model';
 
 export function calculatorMatch(user: User, trip: Trip){
+    let score = 0;
+
+    let breakdwon = {
+        interests : 0,
+        budget: 0,
+        adventure: 0,
+        duration: 0
+    };
+
     
 }
